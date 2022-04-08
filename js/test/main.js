ba293d10e04aa824f0fd0ef52bc75df05297a146
jjuny4720 <jjuny4720@naver.com>
2022-04-08 22:43:07 +0900

    add axios

--- js/test/main.js
+++ js/test/main.js
@@ -219,15 +219,25 @@ boxEl.textContent = 'HEROPY!!';
 console.log(boxEl.textContent); 
 */
 
-//메소드 체이닝
-const a = 'Hello~';
-//split: 문자를 인수 기준으로 쪼개서 배열로 전환
-//reverse: 배열을 뒤집기
-//join : 배열을 인수 기준으로 문자로 병합해 반환.
-const b = a.split('').reverse().join(''); //메소드 체이닝..
+// //메소드 체이닝
+// const a = 'Hello~';
+// //split: 문자를 인수 기준으로 쪼개서 배열로 전환
+// //reverse: 배열을 뒤집기
+// //join : 배열을 인수 기준으로 문자로 병합해 반환.
+// const b = a.split('').reverse().join(''); //메소드 체이닝..
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 
-const boxEl=document.querySelector('.box');
-console.log(boxEl.textContent);
+// const boxEl=document.querySelector('.box');
+// console.log(boxEl.textContent);
+
+import axios from 'axios'
+
+function fetchMovies() {
+  axios.get('https://www.omdbapi.com/?apikey=7035c60c&s=frozen')
+  .then((response) => {
+    console.log(response)
+  })
+}
+fetchMovies()
